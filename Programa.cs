fa444f0db764637571f05056354f0fc949fc21ad
notkahuate <ksbuenors@gmail.com>
2025-04-29 08:57:14 -0500

    feat: :tada: creando juegoi en c#

---
+++ Programa.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adivina
+{
+    public class Programa
+    {
+          public static void Main(string[] args){
+
+          var productosConEtiquetas = new List<(string Producto, List<string> Etiquetas)>
+        {
+            ("Laptop HP", new List<string> { "Electrónica", "Computadores", "Trabajo" }),
+            ("Silla de Oficina", new List<string> { "Muebles", "Confort", "Oficina" }),
+            ("Cafetera Oster", new List<string> { "Cocina", "Electrodomésticos", "Café" })
+        };
+
+        var aplanaretiquetas = productosConEtiquetas.SelectMany(p => p.Etiquetas).Distinct().ToList();
+
+        
+        var random = new Random();
+        var etiquetaOculta = aplanaretiquetas[random.Next(aplanaretiquetas.Count)];
+
+        Console.WriteLine("🎮 ¡Bienvenido al juego 'Encuentra la Etiqueta Oculta'!");
+        Console.WriteLine("Adivina la etiqueta secreta entre las siguientes opciones:\n");
+
+        foreach (var etiqueta in aplanaretiquetas)
+        {
+            Console.WriteLine($"- {etiqueta}");
+        }
+
+        Console.Write("\nIngresa tu intento: ");
+        var intento = Console.ReadLine();
+
+        if (etiquetaOculta.Equals(intento, StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("¡Felicidades! ¡Adivinaste la etiqueta secreta!");
+        }
+        else
+        {
+            Console.WriteLine($"Error, no adivinaste. La etiqueta secreta era: {etiquetaOculta}");
+        }
+
+       }
+    }
+}
